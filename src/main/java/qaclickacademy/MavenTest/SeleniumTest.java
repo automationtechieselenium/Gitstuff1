4fe1748dc763fcf0c727db7cd2c9cdd7886bcddd
automationtechieselenium <sanjitapatnaik224@gmail.com>
2022-02-09 19:31:21 +0530

    first commit from Kolkatta

---
+++ src/main/java/qaclickacademy/MavenTest/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.MavenTest;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomationd");
+	}
+	@Test
+	public void elementsui()
+	{
+		System.out.println("elementsui");
+	}
+}
